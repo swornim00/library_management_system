8a6ca41007666599d07edd9e20311146c598fd4d
Swornim Srestaa <srestaswrnm@gmail.com>
2016-10-24 16:07:57 +0545

    Scaffolding and Login

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('login','Auth\LoginController@showLoginForm');
+Route::get('login','Auth\LoginController@login');
+Route::get('logout','Auth\LoginController@logout');
+
+Route::group(['middleware' => 'auth'],function(){
+
+    Route::get('/home', 'HomeController@index');
+
 });
