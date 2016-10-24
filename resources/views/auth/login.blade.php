13dddf43403e3b4644400cafaea7e2e0a63ec9f1
Swornim Srestaa <srestaswrnm@gmail.com>
2016-10-24 16:17:09 +0545

    Deleted files

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,9 +54,6 @@
                                     Login
                                 </button>
 
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
                             </div>
                         </div>
                     </form>
